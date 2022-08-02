721a5b0e253c6ca23015c8063cd20cd60a6a08a5
Dev Hau <50167033+devhau@users.noreply.github.com>
2022-08-02 23:09:10 +0700

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>BoilerplateX</title>
+        <meta name="description" content="BoilerplateX" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://boilerplatex.com">BoilerplateX!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Github {' '}
+          <code className={styles.code}><a href="https://github.com/BoilerplateX">https://github.com/BoilerplateX</a></code>
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+
+      </footer>
+    </div>
+  )
+}
+
+export default Home
