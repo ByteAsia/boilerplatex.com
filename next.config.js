b77e52edef7fcff161c07ca703f250d91fcadfe5
Dev Hau <50167033+devhau@users.noreply.github.com>
2022-08-02 22:22:29 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -58,6 +58,10 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
   },
+  images: {
+    loader: 'imgix',
+    unoptimized: true,
+  },
   async headers() {
     return [
       {
