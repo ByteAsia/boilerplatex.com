54316ec9598b3b3e9403f566b97d1ea040387bbd
devhau <devhau@users.noreply.github.com>
2022-08-03 02:58:41 +0000

    deploy: 09b314202fda54efc51802006762fe2107738224

--- _next/static/M3EIcSlbjP8damcsaRxqn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c9e31c2156353a4.css","static/chunks/pages/index-25a366febaff224e.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
